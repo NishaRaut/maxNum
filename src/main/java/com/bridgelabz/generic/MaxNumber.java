761f036cf21083732bd32cc909cde2a662256bb8
NishaRaut <nisharaut84@gmail.com>
2020-03-02 17:59:00 +0530

    Added: finding max using generic which extending Comparable

--- src/main/java/com/bridgelabz/generic/MaxNumber.java
+++ src/main/java/com/bridgelabz/generic/MaxNumber.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.generic;
 
 public class MaxNumber {
-    public String getMaxValue(String value1, String value2, String value3) {
-        String max = value1;
+    public static <T extends Comparable<T>> T getMaxValue(T value1, T value2, T value3) {
+        T max = value1;
         if (value2.compareTo(max)>0)
             max = value2;
         if (value3.compareTo(max)>0)
